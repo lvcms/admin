65e6339299e80e0aa2d1c8ff0e2791409c02c5c4
bigrocs <bigrocs@qq.com>
2018-04-08 15:01:13 +0800

    初始化后台管理插件

---
+++ src/adminServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Laracore\Admin;
+
+use Illuminate\Support\ServiceProvider;
+
+class adminServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
