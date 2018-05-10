ac6543b72c73b3148f2d7e6e7348e0bf7dcc523e
bigrocs <bigrocs@qq.com>
2018-05-10 22:04:59 +0800

    增加响应式布局配置数据

--- src/Config/admin/model/system.php
+++ src/Config/admin/model/system.php
@@ -12,27 +12,29 @@
      'path'   => config('admin.uri').'/system',
      // 默认索引页面
      'index' => [
-        'formItem'
+        'form1'
      ],
 
      'layout' => config('admin.layout.row'),
-
-     'formItem' => [
-        'col' => config('admin.layout.col'),
-        'form' => [
-            'mobile' => [
-                'type' => 'text',
-                'title' => '手机',
-                // 字数限制
-                'limit' => 50,
-                'default' => '13954386521',
-            ],
-            'integral' => [
-                'type' => 'text',
-                'title' => '用户积分',
-                'type' => 'textarea',
+     // 索引项目
+     'item' => [
+         'form1' => [
+            'col' => config('admin.layout.col'),
+            'form' => [
+                'mobile' => [
+                    'type' => 'text',
+                    'title' => '手机',
+                    // 字数限制
+                    'limit' => 50,
+                    'default' => '13954386521',
+                ],
+                'integral' => [
+                    'type' => 'text',
+                    'title' => '用户积分',
+                    'type' => 'textarea',
+                ]
             ]
-        ]
+         ]
      ]
 
 ];
