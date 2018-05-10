da4a59d24f5564a2c3ca6d2ea8450b6ef65cfe5a
bigrocs <bigrocs@qq.com>
2018-05-10 21:18:02 +0800

    修改配置文件

--- src/Config/admin/layout/row.php
+++ src/Config/admin/layout/row.php
@@ -5,7 +5,9 @@
  */
 return [
     // 栅格间距，单位 px，左右平分
-    'gutter' => 16,
+    'gutter' => 5,
+    // 布局模式，可选值为flex或不选，在现代浏览器下有效
+    'type' => 'flex',
     // flex 布局下的垂直对齐方式，可选值为top、middle、bottom
     'align' => 'top',
     // flex 布局下的水平排列方式，可选值为start、end、center、space-around、space-between
