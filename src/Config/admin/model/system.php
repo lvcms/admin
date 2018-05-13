564e61a932d6d1c10d41c1ed745b2af8f38cb0c9
bigrocs <bigrocs@qq.com>
2018-05-14 00:43:45 +0800

    修改配置测试

--- src/Config/admin/model/system.php
+++ src/Config/admin/model/system.php
@@ -1,8 +1,9 @@
 <?php
 /**
- * 后台侧边栏菜单
+ * 后台设置
  */
- return [
+
+return [
      // 菜单名称
      'title'   => '系统设置',
      'icon'   => 'fa fa-cog fa-spin',
@@ -10,44 +11,34 @@
      'name'  => config('admin.name').':system',
      //前端路由
      'path'   => config('admin.uri').'/system',
-     // 默认索引页面
-     'index' => [
-        'form1'
-     ],
+     // 默认布局
      'layout' =>[
         [
-            array_merge(config('admin.layout.col'),[
-                'content' => [
-                    [
-                        'style' => 'col',
-                        'span' => 24,
-                        'content' => [
-                            'style' => 'builJson',
-                            'form' => [
-                                'mobile' => [
-                                    'type' => 'text',
-                                    'title' => '手机',
-                                    // 字数限制
-                                    'limit' => 50,
-                                    'default' => '13954386521',
-                                ],
-                                'integral' => [
-                                    'type' => 'text',
-                                    'title' => '用户积分',
-                                    'type' => 'textarea',
-                                ]
-                            ]
-                        ]
-                    ]
-                ]
-            ]),
-        ],
+            'style' => 'row',
+            'config' => config('admin.layout.row'),
+            'content' => [
+                [
+                   'style' => 'col',
+                   'config' => config('admin.layout.col'),
+                   'content' => [
+                       'style' => 'item',
+                       'config' => 'form1'
+                   ]
+               ],
+               [
+                  'style' => 'col',
+                  'config' => config('admin.layout.col'),
+                  'content' => [
+                      'style' => 'item',
+                      'config' => 'form2'
+                  ]
+               ]
+            ],
+        ]
      ],
-     // 'layout' => config('admin.layout.row'),
      // 索引项目
      'item' => [
          'form1' => [
-            'col' => config('admin.layout.col'),
             'form' => [
                 'mobile' => [
                     'type' => 'text',
