c9088168a144faf298c800d8816f652a0d4a70de
bigrocs <bigrocs@qq.com>
2018-05-17 10:35:07 +0800

    修改 配置文件

--- src/App/Models/System.php
+++ src/App/Models/System.php
@@ -6,4 +6,16 @@ use Laracore\Core\App\Models\Model;
 
 class System extends Model
 {
+    protected $config;
+
+    public function setConfig($config)
+    {
+        $this->config = $config;
+        return $this;
+    }
+
+    public function value($config)
+    {
+        return 'values';
+    }
 }
