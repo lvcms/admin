6f2f543e89ccce220e325b22586b379f8792a81c
bigrocs <bigrocs@qq.com>
2018-06-01 17:32:31 +0800

    修改为 trait

--- src/App/Models/Config.php
+++ src/App/Models/Config.php
@@ -2,11 +2,13 @@
 
 namespace Laracore\Admin\App\Models;
 
-use Laracore\Core\App\Models\Model;
+use Illuminate\Database\Eloquent\Model;
+use Laracore\Core\App\Models\Model as LaracoreModel;
 
 class Config extends Model
 {
+    use LaracoreModel;
     public $table = 'admin_configs';
-    
+
     public $timestamps = false;
 }
