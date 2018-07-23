f7e5061aa2aca36f7fb5b5ba773241a7e82c6ffb
bigrocs <bigrocs@qq.com>
2018-07-23 16:18:58 +0800

    删除公共引用

--- src/App/Models/Config.php
+++ src/App/Models/Config.php
@@ -3,11 +3,9 @@
 namespace Laracore\Admin\App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Laracore\Core\App\Models\Model as LaracoreModel;
 
 class Config extends Model
 {
-    use LaracoreModel;
     public $table = 'admin_configs';
 
     public $timestamps = false;
