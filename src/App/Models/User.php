60fd76f51019fad6cec19205eb01e7e4429e16df
bigrocs <bigrocs@qq.com>
2018-07-26 14:43:42 +0800

    增加 user 模块

---
+++ src/App/Models/User.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Laracore\Admin\App\Models;
+
+use Auth;
+use Laracore\Core\App\Models\User as CoreUser;
+
+class User extends CoreUser
+{
+    /**
+     * 后台登录 login
+     */
+    public function login($credentials)
+    {
+        if ($token = $this->attemptToken($credentials)) {
+            $user = Auth::user();
+            return [
+                'status' => 200,
+                'message' => '登录成功',
+                'value' => [
+                    'token' => $token,
+                    'redirect' => '/admin',
+                    'user' => [
+                        'id' => $user->id,
+                        'name' => $user->name,
+                        'email' => $user->email,
+                    ]
+                ]
+            ];
+        } else {
+            abort(401.1, '登录失败');
+        }
+    }
+}
